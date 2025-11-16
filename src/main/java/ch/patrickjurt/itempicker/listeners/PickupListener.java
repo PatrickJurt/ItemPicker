905432b64a26875fa79d8a08f7a1975bea9010ef
PatrickJurt <patrick.jurt@gmx.ch>
2025-11-16 20:10:28 +0100

    create naming for itempickup

--- src/main/java/ch/patrickjurt/itempicker/listeners/PickupListener.java
+++ src/main/java/ch/patrickjurt/itempicker/listeners/PickupListener.java
@@ -1,13 +1,12 @@
 package ch.patrickjurt.itempicker.listeners;
 
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Arrays;
 
 public class PickupListener implements Listener {
 
@@ -17,17 +16,37 @@ public class PickupListener implements Listener {
     @SuppressWarnings("unused")
     @EventHandler
     public void onPickup(EntityPickupItemEvent e) {
-        Player p = e.getEntity().getKiller();
-        if (p != null){
-            String itemName = e.getItem().getItemStack().getType().name();
-            String[] specialItemCategories = {"POTION", "ENCHANTED_BOOK", "GOAT_HORN", "TIPPED_ARROW"};
-            if (Arrays.asList(specialItemCategories).contains(itemName)) {
-                ItemMeta itemMeta = e.getItem().getItemStack().getItemMeta();
-                if (itemMeta != null) {
-                    itemName = itemMeta.toString();
-                }
+        Entity player = e.getEntity();
+        ItemStack itemStack = e.getItem().getItemStack();
+        String itemName = itemStack.getType().name();
+        String name = itemName;
+
+        switch (itemName) {
+            case "POTION", "SPLASH_POTION", "LINGERING_POTION", "TIPPED_ARROW" -> {
+                name = itemName + "_" + getSpecialItem(itemStack, false);
             }
-            p.sendMessage("You picked up " + itemName);
+            case "ENCHANTED_BOOK" -> {
+                name = itemName + "_" + getSpecialItem(itemStack, true);
+            }
+            case "GOAT_HORN" -> {
+                name = getSpecialItem(itemStack, false);
+            }
         }
+        player.sendMessage("You picked up " + name);
+    }
+
+    public String getSpecialItem(ItemStack itemStack, boolean isBook){
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        String marker = "minecraft:";
+        String metaAsString = itemMeta.toString();
+        int index = metaAsString.indexOf(marker);
+        if (index != -1) {
+            if (isBook) {
+                return metaAsString.substring(index + marker.length(), metaAsString.length() - 2).toUpperCase();
+            }else{
+                return metaAsString.substring(index + marker.length(), metaAsString.length() - 1).toUpperCase();
+            }
+        }
+        return "[ERROR] Item has weird meta: " + itemMeta;
     }
 }
