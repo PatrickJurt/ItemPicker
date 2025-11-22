f0fb9efb7ef0641b830f1c80429ddf5447d1d25f
PatrickJurt <patrick.jurt@gmx.ch>
2025-11-22 21:46:32 +0100

    remove unused imports

--- src/main/java/ch/patrickjurt/itempicker/commands/ItemPickerCommand.java
+++ src/main/java/ch/patrickjurt/itempicker/commands/ItemPickerCommand.java
@@ -2,18 +2,13 @@ package ch.patrickjurt.itempicker.commands;
 
 import ch.patrickjurt.itempicker.ChatUtil;
 import ch.patrickjurt.itempicker.ItemPicker;
-import ch.patrickjurt.itempicker.filemanager.FileManager;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import static ch.patrickjurt.itempicker.ItemPicker.MAXITEMS;
-import static ch.patrickjurt.itempicker.ItemPicker.getNewCurrentItem;
 
 public class ItemPickerCommand implements CommandExecutor {
 
